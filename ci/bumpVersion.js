6b7201a6317b6644d1292f23a64b1bcc46ec10f3
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2023-07-10 16:39:09 -0600

    ci: 🎡 more ci logic for bumping

--- ci/bumpVersion.js
+++ ci/bumpVersion.js
@@ -3,6 +3,16 @@ import { readFile } from 'fs/promises';
 
 const IN_DEVELOPMENT = true;
 
+const getMatchInMessages = (messages, matchRegex) => {
+    for (const message of messages) {
+        if (matchRegex.test(message)) return true;
+    }
+}
+
+const currentVersion = execSync('npm pkg get version').toString().replace(/['"]+/g, '')
+
+console.log(currentVersion)
+
 if (!process.env.GITHUB_EVENT_PATH && !IN_DEVELOPMENT) {
     process.exit(1);
 }
@@ -11,11 +21,30 @@ const commits = JSON.parse(
     await readFile(
         new URL(process.env.GITHUB_EVENT_PATH, import.meta.url)
     )
-);
+).commits;
 
-console.log('commits:', commits)
-console.log('commits:', commits.commits)
+const commitMessages = commits.map((commit) => commit.message)
+
+console.log('commitMessages:')
+console.log(commitMessages)
+
+if (getMatchInMessages(commitMessages, /BREAKING/)) {
+    console.log('BUMPING MAJOR')
+    execSync('npm version major --force')
+} else if (getMatchInMessages(commitMessages, /feat/)) {
+    console.log('BUMPING MINOR')
+    execSync('npm version minor --force')
+} else {
+    console.log('BUMPING PATCH')
+    execSync('npm version patch --force')
+}
+
 
 const commitHistory = execSync('git log').toString()
 
 console.log('commitHistory', commitHistory)
+
+
+console.log('PUSHING')
+execSync('git push')
+console.log('PUSHED')
