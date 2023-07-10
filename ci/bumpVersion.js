a08c2efa70df5a438d16550c34b758a07d4d549a
Rudy de Lorenzo <rdelorenzo5@gmail.com>
2023-07-10 16:54:23 -0600

    feat: 🎸 add custom commit message

--- ci/bumpVersion.js
+++ ci/bumpVersion.js
@@ -5,25 +5,31 @@ const IN_DEVELOPMENT = true;
 const BUMP_COMMIT_USERNAME = 'ci'
 const BUMP_COMMIT_EMAIL = 'ci@rudydelorenzo.noreply.ca'
 
+const bump = (type) => {
+    execSync(`npm version ${type} -m "Bump to v. %s" --force`)
+}
+
 const getMatchInMessages = (messages, matchRegex) => {
     for (const message of messages) {
         if (matchRegex.test(message)) return true;
     }
 }
 
-
 if (!process.env.GITHUB_EVENT_PATH && !IN_DEVELOPMENT) {
     process.exit(1);
 }
+
+// Begin script logic proper
+
+// SET USER AND EMAIL
+execSync(`git config user.name ${BUMP_COMMIT_USERNAME}`).toString()
+execSync(`git config user.email ${BUMP_COMMIT_EMAIL}`).toString()
 
 const commits = JSON.parse(
     await readFile(
         new URL(process.env.GITHUB_EVENT_PATH, import.meta.url)
     )
 ).commits;
-
-execSync(`git config user.name ${BUMP_COMMIT_USERNAME}`)
-execSync(`git config user.email ${BUMP_COMMIT_EMAIL}`)
 
 const commitMessages = commits.map((commit) => commit.message)
 
@@ -32,15 +38,14 @@ console.log(commitMessages)
 
 if (getMatchInMessages(commitMessages, /BREAKING/)) {
     console.log('BUMPING MAJOR')
-    execSync('npm version major --force')
+    bump('major')
 } else if (getMatchInMessages(commitMessages, /feat/)) {
     console.log('BUMPING MINOR')
-    execSync('npm version minor --force')
+    bump('minor')
 } else {
     console.log('BUMPING PATCH')
-    execSync('npm version patch --force')
+    bump('patch')
 }
-
 
 console.log('PUSHING')
 execSync('git push')
